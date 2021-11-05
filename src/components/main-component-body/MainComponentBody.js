475e417919a0311024d74e4d9c0c610351f72392
FEDERICK TURIZO <federick.turizo@gmail.com>
2021-11-05 13:06:20 -0500

    Tiny test online, to try the code

--- src/components/main-component-body/MainComponentBody.js
+++ src/components/main-component-body/MainComponentBody.js
@@ -22,10 +22,7 @@ export default function MainComponentBody()
             .then(json => setUserFound(json));*/
 
             fetch('/api/bios/'+userName,{
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
+
             }).then(function(response){
                 console.log(response)
                 return response.json();
@@ -40,10 +37,7 @@ export default function MainComponentBody()
             .then(res => res.json())
             .then(json => setUserFound(json));*/
             fetch('/api/bios/torrenegra',{
-                headers : { 
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }
+
             }).then(function(response){
                 console.log(response)
                 return response.json();
